d2ef8875340ce2b15a4b6625033288505093abb6
victorshehu <44091812+victorshehu@users.noreply.github.com>
2020-07-21 17:01:01 -0400

    Create printFunction.py

---
+++ printFunction.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+
+
+    if n>=1 and n <=150:
+        i = 0
+        for i in range (n):
+            i+=1
+            print(i, end ='')
+            
